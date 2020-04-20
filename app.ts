6678c5cc975f9c0ff7a10bcbdba0c1b5ad3f8941
eydrianJS <adrian.olszowski@wp.pl>
2020-04-20 18:06:28 +0200

    s

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
-console.log(`male japka`)
-console.log(`male japka`)
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+
+console.log(result);
